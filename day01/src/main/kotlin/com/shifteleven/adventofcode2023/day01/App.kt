80dfb784c3d3bb91fc2a521256e349c7a9e17108
K. Adam Christensen <pope@shifteleven.com>
2023-12-19 18:30:04 -0800

    Do Day01's first solution.

--- day01/src/main/kotlin/com/shifteleven/adventofcode2023/day01/App.kt
+++ day01/src/main/kotlin/com/shifteleven/adventofcode2023/day01/App.kt
@@ -1,5 +1,24 @@
 package com.shifteleven.adventofcode2023.day01
 
 fun main() {
-	println("Yo")
+	val lines = object {}.javaClass.getResourceAsStream("/input.txt")!!.bufferedReader().readLines()
+	val sum =
+		lines
+			.parallelStream()
+			// .stream()
+			.mapToInt({ line ->
+				var tens: Int? = null
+				var ones = 0
+				for (c in line) {
+					when (c) {
+						in CharRange('0', '9') -> {
+							tens = tens ?: (c.code - 0x30) * 10
+							ones = c.code - 0x30
+						}
+					}
+				}
+				tens!! + ones
+			})
+			.sum()
+	println(sum)
 }
